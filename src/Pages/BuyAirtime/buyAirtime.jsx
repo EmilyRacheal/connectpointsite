789dcd1e53308979a01036a3498916b65933057e
emily <emilychealy4@gmail.com>
2022-11-10 06:15:24 +0100

    working on the hero section

--- src/Pages/BuyAirtime/buyAirtime.jsx
+++ src/Pages/BuyAirtime/buyAirtime.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styles from "./buyAirtime.module.css"
 import Navbar from "../../components/Navbar"
+import Girl from "../../assets/girl.jpeg"
 
 function BuyAirtime() {
   return (
     <div>
-        {/* <div> */}
         <Navbar />
-        {/* </div> */}
+        <div className='w-full flex'>
+          <div className={`${styles.girlAirtime} w-1/2`}>
+            {/* <img src={Girl} alt="" /> */}
+          </div>
+          <div className='w-1/2'>
+            <div>
+              <h3>Payments</h3>
+              <button>Airtime</button>
+              <button>Internt Bundle</button>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
